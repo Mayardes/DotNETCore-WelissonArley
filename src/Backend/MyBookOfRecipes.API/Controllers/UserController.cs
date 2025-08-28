eded6b4b0b41aa2df8794ec47c104f416d7afb51
Mayardes Oliveira <mayardes.oliveira@globalsys.com.br>
2025-08-28 20:28:22 -0300

    feature: add endpoint user created #closes 30

---
+++ src/Backend/MyBookOfRecipes.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBookOfRecipes.Application.DTO.Request.User.RegisterUser;
+using MyBookOfRecipes.Application.DTO.Response.User.RegisterUser;
+
+namespace MyBookOfRecipes.API.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(RegisterUserResponseDTO), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Register(RegisterUserRequestDTO request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Backend/MyBookOfRecipes.Application/DTO/Request/User/RegisterUser/RegisterUserRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBookOfRecipes.Application.DTO.Request.User.RegisterUser
+{
+    public class RegisterUserRequestDTO
+    {
+        public string Name { get; set;}
+        public string Email { get; set;}
+        public string Password { get; set;}
+    }
+}
