656526653dd9b68184d008a2f1d21170cbb5dd9c
Mayardes Oliveira <mayardes.oliveira@globalsys.com.br>
2025-09-15 18:23:54 -0300

    feature: add unit test using Bogus lib with faker

---
+++ tests/CommonTestUtilities/Requests/User/RegisterUserRequestBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using MyBookOfRecipes.Application.DTO.Request.User.RegisterUser;
+
+namespace CommonTestUtilities.Requests.User
+{
+    public class RegisterUserRequestBuilder
+    {
+        public static RegisterUserRequestDTO Builder()
+        {
+            var faker = new Faker<RegisterUserRequestDTO>("en")
+                .RuleFor(x => x.Name, (f) => f.Person.FirstName)
+                .RuleFor(x => x.Email, (f, x) => f.Internet.Email(x.Name))
+                .RuleFor(x => x.Password, (f) => f.Internet.Password());
+
+            return new RegisterUserRequestDTO
+            {
+                Email = "mayardesoliveira@gmail.com",
+                Name = "Mayardes Oliveira",
+                Password = "356678240"
+            };
+        }
+    }
+}
---
+++ tests/Validation.Tests/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using CommonTestUtilities.Requests.User;
+using MyBookOfRecipes.Application.Validators.UserValidator;
+
+namespace Validation.Tests.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterUserValidator();
+
+            //Act
+            var request = RegisterUserRequestBuilder.Builder();
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
