0fdc5edd0145b552d9640d520de6ce336a801a7a
Mayardes Oliveira <mayardes.oliveira@globalsys.com.br>
2025-09-15 17:36:01 -0300

    feat: migration using FluentMigrator

---
+++ src/Backend/MyBookOfRecipes.Infrastructure/Migrations/Versions/DatabaseVersion.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBookOfRecipes.Infrastructure.Migrations.Versions
+{
+    public abstract class DatabaseVersion
+    {
+        public const int TABLE_USER = 1;
+        //Para cada versão, vamos usar outras propriedades
+    }
+}
---
+++ src/Backend/MyBookOfRecipes.Infrastructure/Migrations/Versions/Version0001.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace MyBookOfRecipes.Infrastructure.Migrations.Versions
+{
+    //[Migration(DatabaseVersion.TABLE_USER, "Update table user")]
+    //public class Version0001 : ForwardOnlyMigration
+    //{
+    //    public override void Up()
+    //    {
+    //        Create.Table("Users")
+    //            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+    //            .WithColumn("CreatedOn").AsDateTime().NotNullable()
+    //            .WithColumn("Active").AsBoolean().NotNullable()
+    //            .WithColumn("Name").AsString(255).NotNullable();
+    //    }
+    //}
+}
